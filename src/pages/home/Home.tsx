60e386a1c7729e15525092ce61ae8107d26ce9b0
fanx <faridazhari46@gmail.com>
2025-11-20 20:25:43 +0700

    add motion dependency

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,9 @@
+import { motion } from "motion/react";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <motion.div className="w-20 h-20 bg-red-500" initial={{ rotate:0 }} animate={{ rotate:360 }} transition={{ duration:100 }}/>
     </div>
   );
 };
